ed36c048f4b002bb5d07d33e1f09861236106e56
Egakrut3 <157879824+Egakrut3@users.noreply.github.com>
2025-09-12 22:49:02 +0300

    Modify documentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
+/*! \file */
+
 #include "Colored_printf.h"
 #include <stdio.h>
 
+/*!
+ *An entry point for the program
+
+ *\return Returns error code of running this program
+ */
 int main()
 {
     colored_printf(GREEN, WHITE, "Hello world!");
